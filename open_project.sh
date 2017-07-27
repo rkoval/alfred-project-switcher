67c25348f6229d885e747a9458a5cca940d1c654
Ryan Koval <ryan.koval@rewardstyle.com>
2017-07-27 16:28:57 -0500

    modified open script to exec

--- open_project.sh
+++ open_project.sh
@@ -5,11 +5,11 @@ project=`basename $query`
 if ! ./focus_window.scpt $project; then
   source ~/.bashrc
   if [ $project == 'simplenote' ]; then
-    ENABLE_NERDTREE=0 mvim -c "set titlestring=$project | SimplenoteList"
+    ENABLE_NERDTREE=0 exec mvim -c "set titlestring=$project | SimplenoteList"
   elif [ $project == 'mysql' ]; then
     file="/nastygoat/dbext-sessions/$(date +%Y-%m-%dT%H:%M:%S%z).sql"
-    cd /nastygoat/dbext-sessions && mvim -c "set titlestring=$project | DBSetOption dbname=@askg" "$file"
+    cd /nastygoat/dbext-sessions && exec mvim -c "set titlestring=$project | DBSetOption dbname=@askg" "$file"
   else
-    cd $query && mvim -c "set titlestring=$project"
+    cd $query && exec mvim -c "set titlestring=$project"
   fi
 fi
